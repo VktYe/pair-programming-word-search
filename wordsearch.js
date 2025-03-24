ffeb224453a6a56596ff11299882c055a2994f8a
VktYe <v.yehorenko@gmail.com>
2025-03-24 17:43:31 -0300

    update function so now it searches words verticaly and reversed

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (!word || typeof word !== 'string') return false;
+  if (!letters || !Array.isArray(letters) || letters.length === 0) return false;
+  
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    const reversedL = l.split('').reverse().join('');
+    if (l.includes(word) || reversedL.includes(word)) return true;
+  }
 
-module.exports = wordSearch
+  const transposed = transpose(letters);
+  const verticalJoin  = transposed.map(ls => ls.join(''));
+  for (let l of verticalJoin) {
+    const reversedL = l.split('').reverse().join('');
+    if (l.includes(word) || reversedL.includes(word)) return true;
+  }
+  return false;
+    
+};
+const transpose = function(matrix) {
+  let result = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    let newArr = [];
+    for (let j = 0; j < matrix.length; j++) {
+      newArr.push(matrix[j][i]);
+    }
+    result.push(newArr);
+  }
+  return result;
+};
+
+module.exports = wordSearch;
